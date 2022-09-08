c2a6c11713eca799c0eefe2bd1c83f15d1505869
Vitor Antero <vitorantero.cruz@gmail.com>
2022-09-08 20:42:28 -0300

    a

---
+++ react-explorer/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Routes from './routes'
+
+const root = ReactDOM.render(<Routes></Routes>, document.getElementById('root'));
